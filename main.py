220e4a3a5b502d64a827832d0fb2437c6e54858b
Arnav Sharma <arnavsharma35@gmail.com>
2023-06-07 10:18:25 +0530

    version 1.0.1

--- main.py
+++ main.py
@@ -17,34 +17,41 @@ st.set_page_config(
 with open('css/style.css') as f:
     d = f.read()
 st.markdown(f'<style>{d}</style>', unsafe_allow_html=True)
-# st.markdown(f'<link rel="icon" type="image/png" sizes="32x32" href="./favicon_io/favicon-32x32.png">',
-#             unsafe_allow_html=True)
 
 models = ['IPL 1st Innings Prediction', 'IPL Win Probability Prediction',
          'T20 1st Innings Prediction', 'T20 Win Probability Prediction']
 
 with open('./script/data.json') as f:
         data = json.load(f)
-IPL_1st_Inn_ModelPred = pickle.load(open('./Models/IPL_1st_Inn_ModelPred.pkl', 'rb'))
-IPL_WinProb_ModelPred = pickle.load(open('./Models/IPL_WinProb_ModelPred.pkl', 'rb'))
-T20_1st_Inn_ModelPred = pickle.load(open('./Models/T20_1st_Inn_ModelPred.pkl', 'rb'))
-T20_WinProb_ModelPred = pickle.load(open('./Models/T20_WinProb_ModelPred.pkl', 'rb'))
+
 
 st.title('Select Cricket Predation Model')
 modelSelected = st.selectbox("", models)
 
 if modelSelected == models[0]:
     st.title(models[0])
+    with open ('./Models/IPL_1st_Inn_ModelPred.pkl', 'rb') as f:
+        IPL_1st_Inn_ModelPred = pickle.load(f)
+    #IPL_1st_Inn_ModelPred = pickle.load(open('./Models/IPL_1st_Inn_ModelPred.pkl', 'rb'))
     ipl_scorePred.iplScorePred(IPL_1st_Inn_ModelPred, data)
 
 if modelSelected == models[1]:
     st.title(models[1])
+    with open ('./Models/IPL_WinProb_ModelPred.pkl', 'rb') as f:
+        IPL_WinProb_ModelPred = pickle.load(f)
+    #IPL_WinProb_ModelPred = pickle.load(open('./Models/IPL_WinProb_ModelPred.pkl', 'rb'))
     ipl_winPred.iplWinPred(IPL_WinProb_ModelPred, data)
 
 if modelSelected == models[2]:
     st.title(models[2])
+    with open ('./Models/T20_1st_Inn_ModelPred.pkl', 'rb') as f:
+        T20_1st_Inn_ModelPred = pickle.load(f)
+    #T20_1st_Inn_ModelPred = pickle.load(open('./Models/T20_1st_Inn_ModelPred.pkl', 'rb'))
     t20_scorePred.t20ScorePred(T20_1st_Inn_ModelPred, data)
 
 if modelSelected == models[3]:
     st.title(models[3])
+    with open ('./Models/T20_WinProb_ModelPred.pkl', 'rb') as f:
+        T20_WinProb_ModelPred = pickle.load(f)
+    #T20_WinProb_ModelPred = pickle.load(open('./Models/T20_WinProb_ModelPred.pkl', 'rb'))
     t20_winPred.t20WinPred(T20_WinProb_ModelPred, data)
